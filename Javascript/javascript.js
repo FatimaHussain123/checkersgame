2f0c2ca8e20dbffb45002bd4f0b386d7eb1eb990
FatimaHussain123 <43818424+FatimaHussain123@users.noreply.github.com>
2019-01-09 16:55:41 +0000

    no message

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,74 +1,47 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var c = document.getElementById("myCanvas");
-    console.log(c);
-    var ctx = c.getContext("2d");
+  var canvas = document.getElementById("myCanvas");
+  console.log(canvas);
+  var ctx = canvas.getContext("2d");
 
-    for ( var x=0; x < 4; x++) {
-        ctx.beginPath()
-        ctx.arc(x*150+37.5, 37.5, 30, 0, 2 * Math.PI);
-        ctx.strokeStyle = "blue";
-        ctx.stroke();
-        ctx.fillStyle = "red";
-        ctx.fill();
-      
-      }
-    for (i=0; i < 4; i++) {
-        ctx.beginPath()
-       ctx.arc(i*150+ 112.5, 112.5, 30, 0, 2 * Math.PI);
-        ctx.strokeStyle = "blue";
-        ctx.stroke();
-     ctx.fillStyle = "red";
-        ctx.fill();
-      
+    var canvas = document.getElementById("myCanvas");
+    
+    var ctx = canvas.getContext("2d");
+    
+    
+    var x = 0
+    var y = 0
+    for(x = 0; x < 8; x++) {
+    
+     for(y = 0; y < 8; y++) {
+       if (y%2) {
+        if(x%2 === 0){
+        ctx.fillStyle = "#000000";
+         ctx.fillRect(x * 100,y * 100,100,100);
+       }
+       } else {
+        if(x%2 !== 0){
+        ctx.fillStyle = "#000000";
+         ctx.fillRect(x * 100,y * 100,100,100);
+       }
        }
        
-       for ( var x=0; x < 4; x++) {
-        ctx.beginPath()
-        ctx.arc(x*150+37.5, 487.5, 30, 0, 2 * Math.PI);
-        ctx.strokeStyle = "eed";
-        ctx.stroke();
-        ctx.fillStyle = "blue";
-        ctx.fill();
       
-      }
-
-      for ( var x=0; x < 8; x++) {
-        ctx.beginPath()
-        ctx.arc(x*150+(-37.5), 562.5, 30, 0, 2 * Math.PI);
-        ctx.strokeStyle = "red";
-        ctx.stroke();
-        ctx.fillStyle = "blue";
-        ctx.fill();
-      
-      }
-        
-      
-      var canvas = document.getElementById("myCanvas");
-      
-      var ctx = canvas.getContext("2d");
-      
-      
-      var x = 0
-      var y = 0
-      for(x = 0; x < 8; x++) {
-      
-       for(y = 0; y < 8; y++) {
-         if (y%2) {
-          if(x%2 === 0){
-          ctx.fillStyle = "#000000";
-           ctx.fillRect(x * 75,y * 75,75,75);
-         }
-         } else {
-          if(x%2 !== 0){
-          ctx.fillStyle = "#000000";
-           ctx.fillRect(x * 75,y * 75,75,75);
-         }
-         }
-         
-        
-       }
-      }
-  });
+     }
+    }
+});
 
 
+var imclueless = $("svg.one > *");
 
+imclueless.on("click", function() {
+  imclueless.removeClass("on");
+  $(this).assClass("on");
+
+});
+
+setInterval(
+  
+)
+
+
+  
